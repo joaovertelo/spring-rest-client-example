9d03f94d93601adf5740d62af5211ec62cadfeea
João Vertelo <joaovertelo@gmail.com>
2023-09-21 18:26:38 -0300

    first commit

---
+++ src/test/java/com/devertelo/springrestclientexample/SpringRestClientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devertelo.springrestclientexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestClientExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
